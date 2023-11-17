90d75caf9f076e55b3df8991a5b5a2eae33af6c7
reivajfly <francisco@fese.mx>
2023-11-17 13:52:19 -0600

    se habilito la opcion para poder regresar en la parte de registro

--- view/datoscomplementarios.php
+++ view/datoscomplementarios.php
@@ -51,10 +51,10 @@ $beneficiario =acces_beneficiario($id);
                     <div class="col-md-12">
                         <ul class="wizard-steps">
                           <li class="finalizado">
-                            <a href="#"><h5>Datos</h5> <span>Personales</span></a>
+                            <a href="datospersonales.php"><h5>Datos</h5> <span>Personales</span></a>
                           </li>
                           <li class="finalizado">
-                            <a href="#"><h5>Datos</h5> <span>Academicos</span></a>
+                            <a href="datosacademicos.php"><h5>Datos</h5> <span>Academicos</span></a>
                           </li>
                           <li class="completed">
                             <a href="#"><h5>Datos</h5> <span>Complementarios</span></a>
