1dd4e85cd0301d36d02d837c7d12c296b56d7222
ANDRES ALEXIS MALFAVAUN <2203024384@alumnos.cua.uam.mx>
2023-11-24 11:21:53 -0600

    modificacion en el template de correo

--- controller/recibe_excel_validando.php
+++ controller/recibe_excel_validando.php
@@ -1,55 +1,65 @@
 <?php
 require('../model/database_emails.php');
-$tipo       = $_FILES['dataCliente']['type'];
-$tamanio    = $_FILES['dataCliente']['size'];
+
 $archivotmp = $_FILES['dataCliente']['tmp_name'];
-$lineas     = file($archivotmp);
-$vacante = isset( $_POST['idvac']) ? $_POST['idvac'] : '';
+$lineas = file($archivotmp);
+$vacante = isset($_POST['idvac']) ? $_POST['idvac'] : '';
 $i = 0;
 
 foreach ($lineas as $linea) {
     $cantidad_registros = count($lineas);
-    $cantidad_regist_agregados =  ($cantidad_registros - 1);
+    $cantidad_regist_agregados = ($cantidad_registros - 1);
 
     if ($i != 0) {
 
-        $datos = explode(",", $linea);
-       
-        $nombre                = !empty($datos[0])  ? ($datos[0]) : '';
-		$correo                = !empty($datos[1])  ? ($datos[1]) : '';
-        // $celular               = !empty($datos[2])  ? ($datos[2]) : '';
-       
-if( !empty($correo) ){
-    // $checkemail_duplicidad = ("SELECT correo FROM clientes WHERE correo='".($correo)."' ");
-            $ca_dupli = search_candidato($correo,$vacante);
+        // Definir los posibles delimitadores
+        $posibles_delimitadores = array(';', ',');
+
+        // Inicializar el delimitador como nulo
+        $delimitador = null;
+
+        // Iterar sobre los posibles delimitadores
+        foreach ($posibles_delimitadores as $d) {
+            // Comprobar si la línea contiene el delimitador actual
+            if (strpos($linea, $d) !== false) {
+                $delimitador = $d;
+                break;
+            }
+        }
+
+        // Si no se encuentra un delimitador válido, mostrar un mensaje de error o manejarlo de acuerdo a tus necesidades
+        if ($delimitador === null) {
+            die("No se pudo determinar el delimitador en la línea: $linea");
+        }
+
+        // Usar el delimitador encontrado para dividir los datos
+        $datos = explode($delimitador, $linea);
+
+        $nombre = !empty($datos[0]) ? trim($datos[0]) : '';
+        $correo = !empty($datos[1]) ? trim($datos[1]) : '';
+
+        if (!empty($correo)) {
+            $ca_dupli = search_candidato($correo, $vacante);
             $cant_duplicidad = mysqli_num_rows($ca_dupli);
-        }   
+        }
 
-//No existe Registros Duplicados
-if ( $cant_duplicidad == 0 ) { 
+        // No existe Registros Duplicados
+        if ($cant_duplicidad == 0) {
+            insert_data($nombre, $correo, $vacante);
+        } else {
+            update_correo($nombre, $correo, $vacante);
+        }
 
-    insert_data($nombre,$correo,$vacante);
-    ?>
-    <script>
-       window.location="../view/consult_vacante_invite.php?vac=<?php echo $vacante ?>";
-    </script>
-<?php
-        
-} 
-/**Caso Contrario actualizo el o los Registros ya existentes*/
-else{
-    update_correo($nombre,$correo,$vacante);
-    ?>
-    <script>
-       window.location="../view/consult_vacante_invite.php?vac=<?php echo $vacante ?>";
-    </script>
-<?php
-    } 
-  }
+        ?>
+        <script>
+            window.location="../view/consult_vacante_invite.php?vac=<?php echo $vacante ?>";
+        </script>
+        <?php
+    }
 
- $i++;
+    $i++;
 }
 
+// Enlace para regresar
+echo '<a href="index.php">Atras</a>';
 ?>
-
-<a href="index.php">Atras</a>
