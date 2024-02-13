ae9501a8315549a1d800bb533640f8493e6f8fdf
Alexis <2203024384@alumnos.cua.uam.mx>
2024-02-12 20:38:22 -0600

    se agrega una conec directa de conexion se agrega el include

--- controller/conec.php
+++ controller/conec.php
@@ -1,8 +1,5 @@
 <?php
-// $servername = "localhost";
-// $dbname = "feseor5_empresarial";
-// $username = "feseor5_empresarial";
-// $password = "HeVr104321";
+
 $servername = "localhost";
 $dbname = "empresarial";
 $username = "root";
--- controller/conexion.php
+++ controller/conexion.php
@@ -1,12 +1,10 @@
 <?php
-//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	// $mysqli = new mysqli("localhost","feseor5_empresarial","HeVr104321","feseor5_empresarial"); 
-	
-	// if(mysqli_connect_errno()){
-	// 	echo 'Conexion Fallida : ', mysqli_connect_error();
-	// 	exit();
-	// }
-	$mysqli = new mysqli("localhost","root","","empresa"); 
+	include 'conec.php';
+// 	$servername = "localhost";
+// $dbname = "empresarial";
+// $username = "root";
+// $password = "";
+	$mysqli = new mysqli($servername,$username,"",$dbname); 
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
