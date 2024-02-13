7f280df5167484fe63bb4dc148374882240f64a7
reivajfly <francisco@fese.mx>
2024-02-12 20:51:49 -0600

    commit para poder realizar un pull

--- controller/conec.php
+++ controller/conec.php
@@ -1,6 +1,11 @@
 <?php
-$servername = "localhost";
-$dbname = "feseor5_staging";
-$username = "feseor5_empresarial";
-$password = "HeVr104321";
+    /*$servername = "localhost";
+    $dbname = "feseor5_staging";
+    $username = "feseor5_empresarial";
+    $password = "HeVr104321";*/
+
+    $servername = "localhost";
+    $dbname = "empresarial";
+    $username = "root";
+    $password = "";
 ?>
